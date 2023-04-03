b86b6bd280e96db6a32d1d87512adc2416292e8d
kaajjaak <yolobroer202@gmail.com>
2023-04-03 18:40:53 +0200

    Wrong import

--- graphs/main_graph.py
+++ graphs/main_graph.py
@@ -1,4 +1,4 @@
 from util.main_grapher import create_graph
-from data.example_data import activities, activity_colors
+from data.data import activities, activity_colors
 
 create_graph(activities, activity_colors)
