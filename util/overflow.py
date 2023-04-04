62b640313cfd146965cfeff1565da222461771c3
kaajjaak <yolobroer202@gmail.com>
2023-04-05 01:11:03 +0200

    I mentioned having to test something more in a previous commit for this file, I now tested it and it was indeed incorrect like I assumed

--- util/overflow.py
+++ util/overflow.py
@@ -14,7 +14,7 @@ def adjust_activities(input_dict):
             else:
                 acts[-1] = (acts[-1][0], round(acts[-1][1] + deviation_hours, 3))
                 print(
-                    f"On {day}, {abs(deviation_hours)} hours need to be  added to {acts[-2][0]}.")
+                    f"On {day}, {abs(deviation_hours)} hours need to be  added to {acts[-1][0]}.")
     return {day: [(act[0], round(act[1], 3)) for act in acts] for day, acts in activities.items()}
 
 
