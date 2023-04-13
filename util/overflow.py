c3690b1ed55e9dfbfa80f1f43c78bd976c197a60
kaajjaak <yolobroer202@gmail.com>
2023-04-13 23:50:43 +0200

    It was still not working correctly but I'm not 100% sure if its perfect now, the changes are just the wording of things though so no critical bugs

--- util/overflow.py
+++ util/overflow.py
@@ -10,11 +10,11 @@ def adjust_activities(input_dict):
             if deviation_hours > 0:
                 acts[-1] = (acts[-1][0], round(acts[-1][1] + deviation_hours, 3))
                 print(
-                    f"On {day}, {deviation_hours} hours need to be removed from to {acts[-1][0]}.")
+                    f"On {day}, {deviation_hours} hours need to be added to {acts[-1][0]}.")
             else:
                 acts[-1] = (acts[-1][0], round(acts[-1][1] + deviation_hours, 3))
                 print(
-                    f"On {day}, {abs(deviation_hours)} hours need to be  added to {acts[-1][0]}.")
+                    f"On {day}, {abs(deviation_hours)} hours need to be removed to {acts[-1][0]}.")
     return {day: [(act[0], round(act[1], 3)) for act in acts] for day, acts in activities.items()}
 
 
